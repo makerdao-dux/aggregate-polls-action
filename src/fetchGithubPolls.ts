6a0ae8975b3374b31c70b64a4d9c3cbd8fe931dc
hernandoagf <hernando.guzman93@gmail.com>
2023-03-08 16:32:04 -0300

    Fetch GH polls in batches to avoid API errors

--- src/fetchGithubPolls.ts
+++ src/fetchGithubPolls.ts
@@ -4,15 +4,27 @@ import { ParsedSpockPoll, PollWithRawMetadata } from './polls'
 export default async function fetchGithubPolls(
   parsedSpockPolls: ParsedSpockPoll[]
 ): Promise<PollWithRawMetadata[]> {
-  const pollsRes = await Promise.allSettled(
-    parsedSpockPolls.map(async ({ pollId, url }) => {
-      const res: AxiosResponse<string> = await axios.get(url)
-      return {
-        pollId,
-        rawMetadata: res.data,
-      }
-    })
-  )
+  const spockPollsInChunks = []
+  const chunkSize = 20
+  const pollsRes = []
+
+  for (let i = 0; i < parsedSpockPolls.length; i += chunkSize) {
+    spockPollsInChunks.push(parsedSpockPolls.slice(i, i + chunkSize))
+  }
+
+  for (let j = 0; j < spockPollsInChunks.length; j++) {
+    const settledPolls = await Promise.allSettled(
+      spockPollsInChunks[j].map(async ({ pollId, url }) => {
+        const res: AxiosResponse<string> = await axios.get(url)
+        return {
+          pollId,
+          rawMetadata: res.data,
+        }
+      })
+    )
+
+    pollsRes.push(...settledPolls)
+  }
 
   const polls = pollsRes
     .map((promise) => (promise.status === 'fulfilled' ? promise.value : null))
